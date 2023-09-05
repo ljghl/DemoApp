20bfc966c7d665cac3d8cab1c7706110b238a227
ljghl <lester.eguaras@ghl.com>
2023-09-05 21:38:44 +0800

    Add vwEmployee

---
+++ SimpleDatabase/dbo/Views/vwEmployee.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW vwEmployee
+AS
+SELECT e.Id,
+       e.FirstName,
+       e.MiddleName,
+       e.LastName,
+       e.Address,
+       e.DepartmentId,
+       e.EmployeeCode,
+       e.Age
+FROM dbo.Employee e;
